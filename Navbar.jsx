7941b05258255be0a7e95858597b12bccd56f40b
Nishchhal-Agarwal <nishchhalrbmhs@gmail.com>
2025-07-02 14:52:46 +0530

    Create Navbar.jsx

---
+++ Navbar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <nav className="bg-blue-600 text-white px-6 py-4 flex justify-between">
+      <h1 className="text-xl font-bold">GovLink</h1>
+      <div className="space-x-4">
+        <Link to="/" className="hover:underline">Home</Link>
+        <Link to="/scheme-finder" className="hover:underline">Scheme Finder</Link>
+        <Link to="/tax-calculator" className="hover:underline">Tax Calculator</Link>
+        <Link to="/login" className="hover:underline">Login</Link>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
